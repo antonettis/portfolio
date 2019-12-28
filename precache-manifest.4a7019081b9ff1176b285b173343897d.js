1a5f69e370457f967cee5bab24c855be71d471be
antonettis <serena.antonetti@gmail.com>
2019-12-29 00:58:49 +0100

    Updates

---
+++ precache-manifest.4a7019081b9ff1176b285b173343897d.js
@@ -0,0 +1,198 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4a2e7b03579aa2339cc83909aaf230d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd7be39516e92467400c",
+    "url": "/static/css/2.e3795789.chunk.css"
+  },
+  {
+    "revision": "79a69c609801c7adb575",
+    "url": "/static/css/main.7681e411.chunk.css"
+  },
+  {
+    "revision": "dd7be39516e92467400c",
+    "url": "/static/js/2.d00e0b04.chunk.js"
+  },
+  {
+    "revision": "ac40add7bfdf23e16c12a47cb83ac085",
+    "url": "/static/js/2.d00e0b04.chunk.js.LICENSE"
+  },
+  {
+    "revision": "79a69c609801c7adb575",
+    "url": "/static/js/main.523e5191.chunk.js"
+  },
+  {
+    "revision": "3db34fda3fd924651c16",
+    "url": "/static/js/runtime-main.32fe3696.js"
+  },
+  {
+    "revision": "090f146b77e0a87dd53628f38fb026ae",
+    "url": "/static/media/Calleis-TheBody.090f146b.jpg"
+  },
+  {
+    "revision": "404ea9e9b5686d5bbf49784b21affa2e",
+    "url": "/static/media/Calleis-TheFace.404ea9e9.jpg"
+  },
+  {
+    "revision": "270800a17167d250269681b709194f8d",
+    "url": "/static/media/Calleis-TheHair.270800a1.jpg"
+  },
+  {
+    "revision": "bc9c96c01659b0224e0159e3ed79ab37",
+    "url": "/static/media/Calleis-Web1.bc9c96c0.jpg"
+  },
+  {
+    "revision": "06636ed290063efcbca940e251bbdb7e",
+    "url": "/static/media/Calleis-Web2.06636ed2.jpg"
+  },
+  {
+    "revision": "8973e6db47e6726ee8c4c1758c28e216",
+    "url": "/static/media/Calleis-Web3.8973e6db.jpg"
+  },
+  {
+    "revision": "f5f9bc9a03a097e1b4f9d8cf31864490",
+    "url": "/static/media/Calleis-Web4.f5f9bc9a.jpg"
+  },
+  {
+    "revision": "1d3e7d9ef32d3050dd0bfd21e918e541",
+    "url": "/static/media/Calleis1.1d3e7d9e.jpg"
+  },
+  {
+    "revision": "fa0088be8e493467bf9b78fde6d62859",
+    "url": "/static/media/Calleis2.fa0088be.jpg"
+  },
+  {
+    "revision": "4139990304b81adca97e71bf55b9200c",
+    "url": "/static/media/Calleis3.41399903.jpg"
+  },
+  {
+    "revision": "067a43edf5379089a5f61486574ba065",
+    "url": "/static/media/EatWelly1.067a43ed.jpg"
+  },
+  {
+    "revision": "4cb909048395b47f9dd37af6a4055f45",
+    "url": "/static/media/EatWelly2.4cb90904.gif"
+  },
+  {
+    "revision": "4c4cb1b3782cd09f361c49616e468a34",
+    "url": "/static/media/EatWelly3.4c4cb1b3.mp4"
+  },
+  {
+    "revision": "554e8af43bded20e7d08a5544a29f2ce",
+    "url": "/static/media/EatWelly3.554e8af4.jpg"
+  },
+  {
+    "revision": "d9d167dd4189b4248782973141e76c7f",
+    "url": "/static/media/EatWelly4.d9d167dd.mp4"
+  },
+  {
+    "revision": "fdae8217ce40746c273f77478ec49587",
+    "url": "/static/media/EatWelly4.fdae8217.jpg"
+  },
+  {
+    "revision": "5e67a8d9c3cce3442e5c80811618f777",
+    "url": "/static/media/Hummingbird1.5e67a8d9.jpg"
+  },
+  {
+    "revision": "68012517fd0ccc44bc4d36c788ae4d9b",
+    "url": "/static/media/Hummingbird2.68012517.jpg"
+  },
+  {
+    "revision": "3bb856b3a0ae2466436fe9d6a9d67df0",
+    "url": "/static/media/Hummingbird3.3bb856b3.jpg"
+  },
+  {
+    "revision": "9a2cb5bc4e7bec0efc22d9cb3785d723",
+    "url": "/static/media/Hummingbird4.9a2cb5bc.jpg"
+  },
+  {
+    "revision": "9350b71e6892341bf136c7f0ae9103c8",
+    "url": "/static/media/Hummingbird5.9350b71e.jpg"
+  },
+  {
+    "revision": "cc3ee282ed458e9df6c4846f44273ded",
+    "url": "/static/media/LaPrinciere-Web1.cc3ee282.jpg"
+  },
+  {
+    "revision": "8ad112ca02efd753f3214ca074c36cfd",
+    "url": "/static/media/LaPrinciere-Web2.8ad112ca.jpg"
+  },
+  {
+    "revision": "ff364f0ac71433537b9a842fe54b2fc0",
+    "url": "/static/media/LaPrinciere-Web3.ff364f0a.jpg"
+  },
+  {
+    "revision": "7b37bef5dfafaf66b6fcc5d3125fcb9b",
+    "url": "/static/media/LaPrinciere-Web4.7b37bef5.jpg"
+  },
+  {
+    "revision": "b4dc53d22dd5fc14fc115c203ed9ef52",
+    "url": "/static/media/LaPrinciere-Web5.b4dc53d2.jpg"
+  },
+  {
+    "revision": "95c9ee81307c0970073dc931a1eb7f10",
+    "url": "/static/media/LaPrinciere1.95c9ee81.jpg"
+  },
+  {
+    "revision": "2839fc4c4b86a33befeec131ac90f3c4",
+    "url": "/static/media/LaPrinciere2.2839fc4c.jpg"
+  },
+  {
+    "revision": "7f8ddaa53cb5a450956ef12010bc80c7",
+    "url": "/static/media/LaPrinciere3.7f8ddaa5.jpg"
+  },
+  {
+    "revision": "9d15c862936399b7ffbc3c25f7390054",
+    "url": "/static/media/LaPrinciere4.9d15c862.jpg"
+  },
+  {
+    "revision": "dd742d01fc43e4a7ea32d71341a53593",
+    "url": "/static/media/LaPrinciere5.dd742d01.jpg"
+  },
+  {
+    "revision": "3b3ce53c3395c97a668267ba4efc2dc6",
+    "url": "/static/media/Soldoa1.3b3ce53c.jpg"
+  },
+  {
+    "revision": "f6c51b51ad57a62cb35415b8e4c03e45",
+    "url": "/static/media/Soldoa2.f6c51b51.jpg"
+  },
+  {
+    "revision": "c459e6f0a8d0c36f48edb0cd831b3594",
+    "url": "/static/media/Soldoa3.c459e6f0.jpg"
+  },
+  {
+    "revision": "510b9916b21c3e6715125d9bc3bba0ca",
+    "url": "/static/media/Soldoa4.510b9916.jpg"
+  },
+  {
+    "revision": "908ffdcf5dd652e7ff2f5ec12ea597a9",
+    "url": "/static/media/calleis.908ffdcf.jpg"
+  },
+  {
+    "revision": "97ebae6b623cfc732eacc876e42c6473",
+    "url": "/static/media/eatwelly.97ebae6b.jpg"
+  },
+  {
+    "revision": "7a525a77094288d53a0f38b6062b462a",
+    "url": "/static/media/hummingbird.7a525a77.jpg"
+  },
+  {
+    "revision": "a36c8be3b484b1831e9be0618eb5abd9",
+    "url": "/static/media/la-princiere.a36c8be3.jpg"
+  },
+  {
+    "revision": "9110ec10518ec4b5abdd237922727c33",
+    "url": "/static/media/logo.9110ec10.svg"
+  },
+  {
+    "revision": "f437da62f86f6a018acae1d3b7ad7371",
+    "url": "/static/media/signature.f437da62.svg"
+  },
+  {
+    "revision": "a7a4279c1139bcf2b54fc791da799895",
+    "url": "/static/media/soldoa.a7a4279c.jpg"
+  }
+]);
